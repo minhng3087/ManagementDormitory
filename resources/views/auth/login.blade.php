f60944ff8eec265db277b4b1f9a7128011b67aae
minh <nguyengiaminh2k@gmail.com>
2020-10-26 00:10:08 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,16 @@
-@extends('layouts.app')
-
+@extends('welcome')
+@section('title','Login')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
+                <div class="card-header">
+                    <div class="d-flex justify-content-between">
+                        <div>{{ __('Login') }}</div>
+                        <div><a class="btn btn-primary" role="button" href="{{ route('register') }}">{{ __('Sign up') }}</a></div>
+                    </div>
+                </div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,4 +1,5 @@
-@extends('layouts.app')
+@extends('welcome')
+@section('title','Email')
 
 @section('content')
 <div class="container">
